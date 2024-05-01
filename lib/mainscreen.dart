3734990465d8870e3cc25bdc6434a76d04dea300
Gyeongjoo Lee <gjlee0802@naver.com>
2024-05-01 23:11:39 +0900

    Add mainscreen.dart

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -13,7 +13,7 @@ class _MainScreenState extends State<MainScreen> {
   //final items = generateItems();
   final controller = TimetableController(
     start: DateUtils.dateOnly(DateTime.now()).subtract(const Duration(days: 7)),
-    initialColumns: 3,
+    initialColumns: 7,
     cellHeight: 100.0,
     startHour: 9,
     endHour: 18,
@@ -41,78 +41,91 @@ class _MainScreenState extends State<MainScreen> {
           IconButton(icon: Icon(Icons.person), onPressed: () {})
         ],
       ),
-      body: Timetable<String>(
-        controller: controller,
-        //items: items,
-        cellBuilder: (datetime) => Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blueGrey, width: 0.2),
-          ),
-          child: Center(
-            child: Text(
-              DateFormat("MM/d/yyyy\nha").format(datetime),
-              style: TextStyle(
-                color: Color(0xff000000 + (0x002222 * datetime.hour) + (0x110000 * datetime.day)).withOpacity(0.5),
+      body: Column(
+        children:[
+          Container(
+            //padding: const EdgeInsets.all(10),
+            height: 600,
+            child: Timetable<String>(
+            controller: controller,
+            //items: items,
+            cellBuilder: (datetime) => Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueGrey, width: 0.2),
               ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-        cornerBuilder: (datetime) => Container(
-          color: Colors.accents[datetime.day % Colors.accents.length],
-          child: Center(child: Text("${datetime.year}")),
-        ),
-        headerCellBuilder: (datetime) {
-          final color = Colors.primaries[datetime.day % Colors.accents.length];
-          return Container(
-            decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(color: color, width: 2)),
-            ),
-            child: Center(
-              child: Text(
-                DateFormat("E\nMMM d").format(datetime),
-                style: TextStyle(
-                  color: color,
+              child: Center(
+                child: Text(
+                  DateFormat("MM/d/yyyy\nha").format(datetime),
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
-          );
-        },
-        hourLabelBuilder: (time) {
-          final hour = time.hour == 12 ? 12 : time.hour % 12;
-          final period = time.hour < 12 ? "am" : "pm";
-          final isCurrentHour = time.hour == DateTime.now().hour;
-          return Text(
-            "$hour$period",
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: isCurrentHour ? FontWeight.bold : FontWeight.normal,
+            cornerBuilder: (datetime) => Container(
+              color: Colors.accents[datetime.day % Colors.accents.length],
+              child: Center(child: Text("${datetime.year}")),
             ),
-          );
-        },
-        itemBuilder: (item) => Container(
-          decoration: BoxDecoration(
-            color: Colors.white.withAlpha(220),
-            borderRadius: BorderRadius.circular(16),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                blurRadius: 4,
-                offset: const Offset(0, 2),
+            headerCellBuilder: (datetime) {
+              final color = Colors.primaries[datetime.day % Colors.accents.length];
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(color: color, width: 2)),
+                ),
+                child: Center(
+                  child: Text(
+                    DateFormat("E\nMMM d").format(datetime),
+                    style: TextStyle(
+                      color: color,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            },
+            hourLabelBuilder: (time) {
+              final hour = time.hour == 12 ? 12 : time.hour % 12;
+              final period = time.hour < 12 ? "am" : "pm";
+              final isCurrentHour = time.hour == DateTime.now().hour;
+              return Text(
+                "$hour$period",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: isCurrentHour ? FontWeight.bold : FontWeight.normal,
+                ),
+              );
+            },
+              /*
+            itemBuilder: (item) => Container(
+              decoration: BoxDecoration(
+                color: Colors.white.withAlpha(220),
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 4,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
               ),
-            ],
-          ),
-          child: Center(
-            child: Text(
-              item.data ?? "",
-              style: const TextStyle(fontSize: 14),
+              child: Center(
+                child: Text(
+                  item.data ?? "",
+                  style: const TextStyle(fontSize: 14),
+                ),
+              ),
+            ),*/
+            nowIndicatorColor: Colors.red,
+            snapToDay: true,
             ),
           ),
-        ),
-        nowIndicatorColor: Colors.red,
-        snapToDay: true,
-      ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("알바생 조회"),
+          ),
+        ]
+      )
     );
   }
 }
