94208cf84dd83fb4394905a13501bde7223b9446
Gyeongjoo Lee <gjlee0802@naver.com>
2024-05-02 17:22:40 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
+import 'timeTable.dart';
+import 'package:calendar_view/calendar_view.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Login(),
+    return CalendarControllerProvider(
+        controller: EventController(),
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: TimeTablePage(), // TODO : change to LoginPage
+        )
     );
   }
 }
